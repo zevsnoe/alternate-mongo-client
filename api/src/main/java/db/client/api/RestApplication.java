13df07f411ca45aaf8a69d1702818825a721e697
alexbz <bezugliy.alex@gmail.com>
2018-05-19 01:39:09 +0300

    temporarely duplicate application properties

--- api/src/main/java/db/client/api/RestApplication.java
+++ api/src/main/java/db/client/api/RestApplication.java
@@ -1,5 +1,6 @@
 package db.client.api;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -7,4 +8,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @ComponentScan(basePackages = {"db.client.api", "db.client.app"})
 @EnableSwagger2
-public class RestApplication {}
+public class RestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
+	}
+}
